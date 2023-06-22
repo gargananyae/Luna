f0851e4dca15161cc7ea94c80e4387fabf33884d
compli <shirothegamer69@gmail.com>
2023-06-22 13:44:49 +0530

    slider fixed

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,44 +1,44 @@
-import React from 'react'
-import {Swiper, SwiperSlide} from "swiper/react";
-import {Pagination, Navigation} from "swiper";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
 
 // import swiper styles
 
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import './Slider.css';
-import { SliderProducts } from '../../data/products';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./Slider.css";
+import { ProductsData } from "../../data/products";
 
-
- const Slider = () => {
+const Slider = () => {
   return (
     <div className="s-container">
-      <Swiper  
-        modules={[Pagination, Navigation]} 
+      <Swiper
+        modules={[Pagination, Navigation]}
         className="mySwiper"
         loopFillGroupWithBlank={true}
-        navigation={true} 
-        slidesPerView={3} spaceBetween={40} slidesPerGroup={2} loop={true}>
-        {SliderProducts.map((slide, i) => (
-          
-            
-            <SwiperSlide>
-               <div className="left-s">
-                <div className="name">
-                    <span>{slide.name}</span>
-                    <span>{slide.detail}</span>
-                </div>
-                <span>{slide.price}$</span>
-                <div>Shop Now</div>
-               </div>
-               <img src={slide.img}  alt="product" className="img-p"/>
-            </SwiperSlide>
-            
-          ))}
-        </Swiper>
+        navigation={true}
+        slidesPerView={3}
+        spaceBetween={40}
+        slidesPerGroup={2}
+        loop={true}
+      >
+        {ProductsData.map((slide, i) => (
+          <SwiperSlide>
+            <div className="left-s">
+              <div className="name">
+                <span>{slide.name}</span>
+                <span>{slide.detail}</span>
+              </div>
+              <span>{slide.price}$</span>
+              <div>Shop Now</div>
+            </div>
+            <img src={slide.img} alt="product" className="img-p" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
-}
+};
 
-export default Slider
+export default Slider;
